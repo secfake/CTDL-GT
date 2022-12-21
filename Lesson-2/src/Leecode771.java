e273a982bb4dafe3bec19825f6c92ff231206de3
Chu Xuân Vĩnh <xuanvinhln@gmail.com>
2022-12-21 16:55:32 +0700

    Update Leecode771.java

--- Lesson-2/src/Leecode771.java
+++ Lesson-2/src/Leecode771.java
@@ -1,9 +1,9 @@
 public class Leecode771 {
     public int numJewelsInStones(String jewels, String stones) {
         int count = 0;
-        for (int i = 0; i < jewel.length(); i++) {
+        for (int i = 0; i < jewels.length(); i++) {
             for (int j = 0; j < stones.length(); j++) {
-                if (jewel.charAt(i) == stones.charAt(j)) {
+                if (jewels.charAt(i) == stones.charAt(j)) {
                     count++;
                 }
             }
